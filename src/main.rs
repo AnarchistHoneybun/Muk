176d7c24afa972f569632a6fec9dd58801a7bf1e
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-03-05 17:28:35 +0530

    dyn-size images, better colors

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,21 @@ use reqwest::get;
 
 fn get_color_for_type(pokemon_type: &str) -> Color {
     match pokemon_type {
-        "Fire" => Color::Red,
-        "Water" => Color::Blue,
-        "Grass" => Color::Green,
-        "Electric" => Color::Yellow,
-        "Psychic" => Color::Magenta,
-        "Poison" => Color::Magenta,
-        "Ice" => Color::Cyan,
-        "Dragon" => Color::LightRed,
-        "Dark" => Color::Black,
-        "Fairy" => Color::White,
+        "Fire" => Color::Rgb(255, 102, 0),
+        "Water" => Color::Rgb(51, 204, 255),
+        "Grass" => Color::Rgb(0, 153, 0),
+        "Electric" => Color::Rgb(255, 255, 0),
+        "Psychic" => Color::Rgb(102, 102, 153),
+        "Poison" => Color::Rgb(102, 0, 204),
+        "Ice" => Color::Rgb(102, 255, 255),
+        "Dragon" => Color::Rgb(204, 0, 0),
+        "Bug" => Color::Rgb(102, 102, 51),
+        "Fighting" => Color::Rgb(153, 204, 255),
+        "Flying" => Color::Rgb(255, 204, 0),
+        "Ghost" => Color::Rgb(102, 0, 255),
+        "Ground" => Color::Rgb(153, 102, 51),
+        "Rock" => Color::Rgb(153, 102, 0),
+        "Normal" => Color::Rgb(255, 255, 255),
         _ => Color::White, // Default color for unknown types
     }
 }
@@ -186,17 +191,17 @@ async fn main() -> Result<()> {
             let dim;
             if(area.height*3 < area.width) {
                 dir = Direction::Horizontal;
-                dim = area.height;
+                dim = 50;
             }else{
                 dir = Direction::Vertical;
-                dim = area.width;
+                dim = 60;
             }
 
             let pokedex = Layout::default()
                 .direction(dir)
                 .constraints(vec![
-                    Constraint::Percentage(50),
-                    Constraint::Percentage(50),
+                    Constraint::Percentage(dim),
+                    Constraint::Percentage(100-dim),
                     Constraint::Min(0),
                 ])
                 .split(area);
@@ -236,7 +241,7 @@ async fn main() -> Result<()> {
                 "display.png",
                 &mut buffer,
                 &RenderOptions::new()
-                    .width(40)
+                    .height(screen[1].height as u32)
                     // .charset(&[".","@" ,"#"]),
                     .charset(BLOCK)
             )
